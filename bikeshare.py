7a5a82198ce4e1a4b606b7b445fdfed921024a69
Faisal Fayyaz <ffayyaz@gmail.com>
2019-11-07 17:38:43 +0400

    refactor: Added comments to CITYDATA dictionary

--- bikeshare.py
+++ bikeshare.py
@@ -11,6 +11,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# Dictionary defining city names to city data files
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
